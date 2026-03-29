f50c085a95e1e932095b54fd197b6ec164584389
Ivan Dimitrov <ivandm2509@gmail.com>
2026-03-29 19:06:26 +0200

    Add benchmark script + ReadME instructions

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
 import torch
 import transformers
-def main():
-    print(f"PyTorch version: {torch.__version__} and cuda available: {torch.cuda.is_available()}")
+from accelerate import Accelerator
+from sympy import Dict
+from transformers import AutoModelForImageTextToText, AutoProcessor
+
+
+def setup():
+    print(
+        f"PyTorch version: {torch.__version__} and cuda available: {torch.cuda.is_available()}"
+    )
     print(f"Transformers version: {transformers.__version__}")
 
 
+def build_message(image_path: str, prompt: str) -> Dict:
+    return [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": f"{image_path}",
+                },
+                {
+                    "type": "text",
+                    "text": f"{prompt}",
+                },
+            ],
+        }
+    ]
+
+
 if __name__ == "__main__":
-    main()
+    setup()
+
+    device = Accelerator().device
+    model = AutoModelForImageTextToText.from_pretrained(
+        "HuggingFaceTB/SmolVLM-500M-Instruct",
+        dtype=torch.bfloat16,
+        attn_implementation="eager",
+    ).to(device)
+
+    processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-500M-Instruct")
+
+    img_path = "data/benchmark/coco_subset/COCO_val2014_000000001171.jpg"
+    prompt = f"Answer with only 'yes' or 'no'.\nQuestion: Is there a locomotive in the image?"
+    message = build_message(img_path, prompt)
+
+    text = processor.apply_chat_template(
+        message,
+        tokenize=True,
+        add_generation_prompt=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(device)
+
+    input_len = len(text.input_ids[0])
+
+    with torch.no_grad():
+        generated_ids = model.generate(**text, max_new_tokens=200)
+    generated_texts = processor.batch_decode(
+        generated_ids[:, input_len:], skip_special_tokens=True
+    )
+
+    print(generated_texts)
